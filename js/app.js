0383ff5595c9a374c0bd8aff638a57a98040a173
Renato Chocce Palacios <renatochocce758@gmail.com>
2024-12-27 20:47:34 -0300

    funcion mostrar tarea

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const formularioTarea = document.getElementById('formulario-tarea');
 const listaTarea = document.getElementById('lista-tarea');
 const buttonRealizado = document.getElementsByClassName('button-realizado');
 const buttonEliminar = document.getElementsByClassName('button-eliminar');
-const tareasArray =  JSON.parse(localStorage.getItem('tareas')) || [];
+const tareasArray = JSON.parse(localStorage.getItem('tareas')) || [];
 
 
 const agregarTarea = (e) => {
@@ -45,12 +45,39 @@ const localStorageTareas = () => {
     localStorage.setItem('tareas', JSON.stringify(tareasArray));
 }
 
+const mostrarTareas = () => {
+    tareasArray.forEach(tarea => {
+        const tareaLi = document.createElement('li');
+        tareaLi.classList.add('flex', 'items-center', 'justify-between', 'p-2');
+
+        tareaLi.innerHTML = `
+            ${tarea}
+            <div class="flex gap-2">
+                <button class="button-realizado rounded-full bg-green-500 p-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                    </svg>
+                </button>
+                <button class="button-eliminar  bg-red-600 p-1 text-white rounded-full"
+                    aria-label="Eliminar tarea"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+        `;
+
+        listaTarea.appendChild(tareaLi);
+    });
+}
 
 console.log(buttonRealizado);
 console.log(buttonEliminar);
 
 formularioTarea.addEventListener('submit', agregarTarea);
 
-buttonEliminar.forEach(button => {
+/* buttonEliminar.forEach(button => {
     button.addEventListener('click', eliminarTarea);
-});
+}); */
+localStorageTareas();
+mostrarTareas();
