aea05536f1e6ec768dc6f53e4da0024eb840040e
Renato Chocce Palacios <renatochocce758@gmail.com>
2024-12-28 23:26:49 -0300

    marcar tarea realizada hecho

--- js/app.js
+++ js/app.js
@@ -9,32 +9,19 @@ const agregarTarea = (e) => {
     e.preventDefault();
 
     const tareaValor = inputTarea.value.trim();
-    const tarea = document.createElement('li');
-    if (tareaValor === '') return
+    if (tareaValor === '') return;
 
-    tarea.classList.add('flex', 'items-center', 'justify-between', 'p-2');
-    tarea.innerHTML = `
-        ${tareaValor}
-        <div class="flex gap-2">
-            <button class="button-realizado rounded-full bg-green-500 p-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                </svg>
-            </button>
-            <button class="button-eliminar  bg-red-600 p-1 text-white rounded-full" aria-label="Eliminar tarea">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-            </button>
-        </div>
-    `;
-    tareasArray.push(tareaValor);
+    const tareaObject = {
+        id: Date.now(), // Genera un ID único para cada tarea
+        tarea: tareaValor,
+        realizada: false
+    }
+
+    tareasArray.push(tareaObject);
+    inputTarea.value = '';
     localStorageTareas();
-    console.log(tareasArray)
-    inputTarea.value = ''
-    listaTarea.appendChild(tarea);
+    mostrarTareas()
+    console.log(tareaObject);
 }
 
 const eliminarTarea = () => {
@@ -46,14 +33,18 @@ const localStorageTareas = () => {
 }
 
 const mostrarTareas = () => {
+    listaTarea.innerHTML = '';
     tareasArray.forEach(tarea => {
         const tareaLi = document.createElement('li');
         tareaLi.classList.add('flex', 'items-center', 'justify-between', 'p-2');
+        if (tarea.realizada) tareaLi.classList.add('line-through');
+        
+        tareaLi.dataset.id = tarea.id; // Almacena el ID de la tarea en el atributo data-id
 
         tareaLi.innerHTML = `
-            ${tarea}
+            ${tarea.tarea}
             <div class="flex gap-2">
-            <button class="button-realizado rounded-full bg-green-500 p-1" onclick="realizarTarea(this)">
+            <button class="button-realizado rounded-full bg-green-500 p-1" aria-label="Marcar como realizada" data-id="${tarea.id}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                 </svg>
@@ -67,16 +58,21 @@ const mostrarTareas = () => {
         `;
 
         listaTarea.appendChild(tareaLi);
+        const buttonRealizado = tareaLi.querySelector('.button-realizado');
+        buttonRealizado.addEventListener('click', () => realizarTarea(tarea));
     });
 }
 const buttonRealizado = document.getElementsByClassName('button-realizado');
 
 // console.log(buttonRealizado)
 const realizarTarea = (tarea) => {
-
-    const liTarea = tarea.parentElement.parentElement;    
-    liTarea.classList.toggle('line-through');
-    console.log('realizando tarea...')
+    // 'tarea.realizada' puede ser true o false
+    // Si 'realizada' es false (tarea no está hecha), la expresión '!' lo convertirá a true (tarea está hecha)
+    // Si 'realizada' es true (tarea está hecha), la expresión '!' lo convertirá a false (tarea no está hecha)
+    // La línea completa asigna a 'tarea.realizada' el valor opuesto al que tenía anteriormente.
+    tarea.realizada = !tarea.realizada; // Alterna el estado
+    localStorageTareas();
+    mostrarTareas();
 }
 /* console.log(buttonRealizado);
 console.log(buttonEliminar); */
