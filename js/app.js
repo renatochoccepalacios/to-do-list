13280a0823c10df79a4b68eba6463b58543c642c
Renato Chocce Palacios <renatochocce758@gmail.com>
2025-01-17 14:54:38 -0300

    cambiando el innerHTML a appendChild

--- js/app.js
+++ js/app.js
@@ -61,48 +61,67 @@ const mostrarTareas = () => {
         tareaLi.classList.add('flex', 'items-end', 'justify-between', 'flex-col', 'gap-4', 'overflow-auto');
         tareaLi.dataset.id = tarea.id; // Almacena el ID de la tarea en el atributo data-id
 
-        const hora = new Date(tarea.id);
+        const hora = new Date(tarea.id); // Se crea un nuevo objeto de fecha usando el ID de la tarea, que es un número de milisegundos desde 1970.
 
-        tareaLi.innerHTML = `
-        ${hora.getDate()}/${hora.getMonth() + 1}/${hora.getFullYear()}  -  ${hora.getHours()}:${hora.getMinutes().toString().padStart(2, '0')}
-        <div class="flex w-full justify-between items-center p-2 rounded-lg ${tarea.realizada ? 'bg-verde-pastel line-through' : 'bg-naranja-pastel'} gap-3 overflow-x-auto">
-        ${tarea.text}
-        <div class="flex gap-2">
-            <button class="button-editar bg-blue-950 text-white p-2 rounded-md" aria-label="Editar tarea">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                    </svg>                          
-            </button>
-        <button class="button-realizado ${tarea.realizada ? 'bg-green-500' : 'bg-orange-500 text-white'} p-2 rounded-md" aria-label="Marcar como realizada" data-id="${tarea.id}">
-        ${tarea.realizada ? ` 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                </svg>
-            ` : `
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 ">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            `}
-            </button>
-            <button class="button-eliminar bg-red-600 p-2 rounded-md text-white" aria-label="Eliminar tarea">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-                </svg>
-            </button>
-        </div>
-        </div>
+        const fechaTexto = document.createTextNode(`${hora.getDate()}/${hora.getMonth() + 1}/${hora.getFullYear()}  -  ${hora.getHours()}:${hora.getMinutes().toString().padStart(2, '0')}`);
+        tareaLi.appendChild(fechaTexto);
+
+        const divTarea = document.createElement('div');
+        divTarea.classList.add('flex', 'w-full', 'justify-between', 'items-center', 'p-2', 'rounded-lg', tarea.realizada ? 'bg-verde-pastel' : 'bg-naranja-pastel', 'gap-3', 'overflow-x-auto');
+        tareaLi.appendChild(divTarea);
+
+        const tareaTexto = document.createTextNode(tarea.text);
+        divTarea.appendChild(tareaTexto);
+
+        const divBotones = document.createElement('div');
+        divBotones.classList.add('flex', 'gap-2');
+        divTarea.appendChild(divBotones);
+
+        const buttonEditar = document.createElement('button');
+        buttonEditar.classList.add('button-editar', 'bg-blue-950', 'text-white', 'p-2', 'rounded-md');
+        buttonEditar.setAttribute('aria-label', 'Editar tarea');
+        buttonEditar.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg>
         `;
+        divBotones.appendChild(buttonEditar);
+
+        const buttonRealizado = document.createElement('button');
+        buttonRealizado.classList.add('button-realizado', tarea.realizada ? 'bg-green-500' : 'bg-orange-500', 'text-white', 'p-2', 'rounded-md');
+        buttonRealizado.setAttribute('aria-label', 'Marcar como realizada');
+        buttonRealizado.setAttribute('data-id', tarea.id);
+        buttonRealizado.innerHTML = tarea.realizada ? `
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+            </svg>
+        ` : `
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        `;
+        divBotones.appendChild(buttonRealizado);
+
+        const buttonEliminar = document.createElement('button');
+        buttonEliminar.classList.add('button-eliminar', 'bg-red-600', 'p-2', 'rounded-md', 'text-white');
+        buttonEliminar.setAttribute('aria-label', 'Eliminar tarea');
+        buttonEliminar.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+            </svg>
+        `;
+        divBotones.appendChild(buttonEliminar);
 
         listaTarea.appendChild(tareaLi);
 
-        const buttonRealizado = tareaLi.querySelector('.button-realizado');
-        buttonRealizado.addEventListener('click', () => realizarTarea(tarea));
+        const realizado = tareaLi.querySelector('.button-realizado');
+        realizado.addEventListener('click', () => realizarTarea(tarea));
 
-        const buttonEliminar = tareaLi.querySelector('.button-eliminar');
-        buttonEliminar.addEventListener('click', () => eliminarTarea(tarea));
+        const eliminar = tareaLi.querySelector('.button-eliminar');
+        eliminar.addEventListener('click', () => eliminarTarea(tarea));
 
-        const buttonEditar = tareaLi.querySelector('.button-editar');
-        buttonEditar.addEventListener('click', () => editarTarea(tarea));
+        const editar = tareaLi.querySelector('.button-editar');
+        editar.addEventListener('click', () => editarTarea(tarea));
     });
 
     if (tareasArray.length === 0) {
